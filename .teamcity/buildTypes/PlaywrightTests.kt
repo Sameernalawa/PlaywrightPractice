bdef7c981a03727802150143707635d10d935fa8
Sameer <sameernalawa>
2025-11-21 11:46:48 +0530

    small fix

--- .teamcity/buildTypes/PlaywrightTests.kt
+++ .teamcity/buildTypes/PlaywrightTests.kt
@@ -5,16 +5,16 @@ import jetbrains.buildServer.configs.kotlin.v2019_2.*
 class PlaywrightTests : BuildType({
     name = "Playwright Tests"
 
+    // Optional: attach a VCS root if already created in TeamCity
+    // Replace "PlaywrightPractice_GitHub" with your actual VCS root name
     vcs {
-        root(DslContext.settingsRoot("https://github.com/Sameernalawa/PlaywrightPractice.git#refs/heads/main"))
+        root(DslContext.createVcsRoot("https://github.com/Sameernalawa/PlaywrightPractice.git"))
     }
 
     steps {
         script {
             name = "Install Dependencies"
-            scriptContent = """
-                npm install
-            """.trimIndent()
+            scriptContent = "npm install"
         }
         script {
             name = "Run Playwright Tests"
