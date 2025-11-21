9aa2eca23108027f0d18450a2302a8a5b497171c
Sameer <sameernalawa>
2025-11-21 16:26:00 +0530

    fix

--- .teamcity/buildTypes/PlaywrightTests.kt
+++ .teamcity/buildTypes/PlaywrightTests.kt
@@ -1,8 +1,9 @@
 package buildTypes
 
 import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.*
 
-object PlaywrightTests : BuildType({
+class PlaywrightTests : BuildType({
     name = "Playwright Tests"
 
     steps {
--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,9 +1,14 @@
 import jetbrains.buildServer.configs.kotlin.*
 import jetbrains.buildServer.configs.kotlin.projectFeatures.*
 import jetbrains.buildServer.configs.kotlin.buildSteps.*
+import jetbrains.buildServer.configs.kotlin.vcs.*
+import buildTypes.PlaywrightTests
 
 version = "2024.03"
 
 project {
+    id("PlaywrightPractice")
+    name = "PlaywrightPractice"
+
     buildType(PlaywrightTests)
 }
