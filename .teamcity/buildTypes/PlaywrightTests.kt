88aa1be12d863b2a15599c67ae551c639b30b163
Sameer <sameernalawa>
2025-11-22 11:38:34 +0530

    fix

--- .teamcity/buildTypes/PlaywrightTests.kt
+++ .teamcity/buildTypes/PlaywrightTests.kt
@@ -11,6 +11,22 @@ object PlaywrightTests : BuildType({
         script {
             name = "Install & Test"
             scriptContent = """
+            sudo apt-get update
+            sudo apt-get install -y \
+                libgtk-3-0 \
+                libgbm-dev \
+                libnss3 \
+                libasound2 \
+                libxshmfence1 \
+                libx11-xcb1 \
+                libxcomposite1 \
+                libxdamage1 \
+                libxfixes3 \
+                libxrandr2 \
+                libxtst6 \
+                libatk1.0-0 \
+                libatk-bridge2.0-0 \
+                libatspi2.0-0
                 npm install
                 npx playwright install
                 npx playwright test
