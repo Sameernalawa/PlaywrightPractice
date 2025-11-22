cabb9a83b858c62ff5c4b7f8507d0807e53ab856
Sameer <sameernalawa>
2025-11-22 11:46:34 +0530

    fix

--- .teamcity/buildTypes/PlaywrightTests.kt
+++ .teamcity/buildTypes/PlaywrightTests.kt
@@ -3,34 +3,47 @@ package buildTypes
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 
-
 object PlaywrightTests : BuildType({
     name = "Playwright Tests"
 
     steps {
         script {
-            name = "Install & Test"
+            name = "Install Dependencies"
             scriptContent = """
-            sudo apt-get update
-            sudo apt-get install -y \
-                libgtk-3-0 \
-                libgbm-dev \
-                libnss3 \
-                libasound2 \
-                libxshmfence1 \
-                libx11-xcb1 \
-                libxcomposite1 \
-                libxdamage1 \
-                libxfixes3 \
-                libxrandr2 \
-                libxtst6 \
-                libatk1.0-0 \
-                libatk-bridge2.0-0 \
-                libatspi2.0-0
+                apt-get update
+                apt-get install -y \
+                    libgtk-3-0 \
+                    libgtk-4-1 \
+                    libgbm-dev \
+                    libnss3 \
+                    libasound2 \
+                    libxshmfence1 \
+                    libx11-xcb1 \
+                    libxcomposite1 \
+                    libxdamage1 \
+                    libxfixes3 \
+                    libxrandr2 \
+                    libxtst6 \
+                    libatk1.0-0 \
+                    libatk-bridge2.0-0 \
+                    libatspi2.0-0 \
+                    libvpx7 \
+                    libopus0 \
+                    gstreamer1.0-plugins-base \
+                    gstreamer1.0-plugins-good \
+                    gstreamer1.0-libav
+
                 npm install
-                npx playwright install
+                npx playwright install --with-deps
+            """.trimIndent()
+        }
+
+        script {
+            name = "Run Playwright Tests"
+            workingDir = "%teamcity.build.checkoutDir%"
+            scriptContent = """
                 npx playwright test
-            """
+            """.trimIndent()
         }
     }
 
