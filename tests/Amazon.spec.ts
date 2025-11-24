680a8fff934b0fd0b27c9697c87aeb170f7af635
Sameer <sameernalawa>
2025-11-24 21:28:56 +0530

    newScript

---
+++ tests/Amazon.spec.ts
@@ -0,0 +1,18 @@
+import {test,expect} from '@playwright/test';
+
+
+
+test("amazon login to logout", async({page})=> {
+
+await page.goto("https://www.amazon.in/?");
+await page.locator("#twotabsearchtextbox").fill("redmi 13 pro");
+await page.locator("[type='submit']").click();
+const productLocator = page.getByText("Redmi 13 5G Prime Edition, Hawaiian Blue, 8GB+128GB").first();
+await productLocator.waitFor({ state: 'visible', timeout: 10000 });
+await productLocator.click();
+const mobile_test = await productLocator.textContent();
+console.log(mobile_test);
+
+
+
+});
